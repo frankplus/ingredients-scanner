e7018f15350329fcbffc2341efc48a2cbba78dd2
Francesco Pham <pham.francesco@gmail.com>
2019-03-12 18:27:59 +0100

    Add firebase and launch OCR text extraction

--- app/src/main/java/com/example/francesco/ingredientsscanner/ResultActivity.kt
+++ app/src/main/java/com/example/francesco/ingredientsscanner/ResultActivity.kt
@@ -2,12 +2,17 @@ package com.example.francesco.ingredientsscanner
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+
+private const val TAG = "ResultActivity"
 
 class ResultActivity : AppCompatActivity() {
 
@@ -22,10 +27,7 @@ class ResultActivity : AppCompatActivity() {
         }
 
         val picturePath = intent.getStringExtra(EXTRA_PICTUREPATH)
-        if(picturePath != null) {
-            val bitmapPicture = loadBitmapFromFile(picturePath)
-            takenPictureView.setImageBitmap(bitmapPicture)
-        }
+        if(picturePath != null) launchOCR(picturePath)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,5 +44,21 @@ class ResultActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun launchOCR(picturePath: String) {
+        val bitmapPicture = loadBitmapFromFile(picturePath)
+        takenPictureView.setImageBitmap(bitmapPicture)
+
+        val image = FirebaseVisionImage.fromBitmap(bitmapPicture)
+        val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+
+        val result = detector.processImage(image)
+            .addOnSuccessListener { firebaseVisionText ->
+                Log.d(TAG, firebaseVisionText.text)
+            }
+            .addOnFailureListener {
+                Log.e(TAG, "text extraction failed")
+            }
     }
 }
