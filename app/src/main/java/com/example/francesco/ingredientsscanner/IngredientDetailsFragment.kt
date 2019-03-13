35c8c7704eccbf7080c2ca94ba497659c054f9d5
Francesco Pham <pham.francesco@gmail.com>
2019-03-13 18:16:03 +0100

    WIP: ingredients details fragment

---
+++ app/src/main/java/com/example/francesco/ingredientsscanner/IngredientDetailsFragment.kt
@@ -0,0 +1,165 @@
+package com.example.francesco.ingredientsscanner
+
+import android.app.SearchManager
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import kotlinx.android.synthetic.main.fragment_ingredient_details.view.*
+
+import org.json.JSONException
+import org.json.JSONObject
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+
+private const val TAG = "IngredDetailsFragment"
+
+const val ARG_NAME = "name"
+const val ARG_DESCRIPTION = "description"
+const val ARG_FUNCTION = "function"
+
+
+/**
+ * Dialog Fragment that shows details about the ingredient selected such as name, description,
+ * function and an extract from wikipedia.
+ * @author Francesco Pham
+ */
+class IngredientDetailsFragment : DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_ingredient_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val inciName = arguments?.run{getString(ARG_NAME)}
+        val description = arguments?.run{getString(ARG_DESCRIPTION)}
+        val function = arguments?.run{getString(ARG_FUNCTION)}
+
+        //set on click listener on search button
+        val searchButton = view.findViewById<Button>(R.id.searchButton)
+        searchButton.setOnClickListener {
+            //credit to Luca Moroldo
+            val webSearchIntent = Intent(Intent.ACTION_WEB_SEARCH)
+            webSearchIntent.putExtra(SearchManager.QUERY, inciName)
+            startActivity(webSearchIntent)
+        }
+
+        //set close button listener
+        val closeButton = view.closeButton
+        closeButton.setOnClickListener {dismiss()}
+
+        // show ingredient information
+        val nameView = view.inciNameView
+        nameView.text = inciName
+
+        val descriptionView = view.descriptionView
+        descriptionView.text = description
+
+        val functionView = view.functionView
+        functionView.text = function
+
+        val wikipediaView = view.wikipediaView
+
+        // Get a RequestQueue
+        RequestQueueSingleton.getInstance(getActivity()).getRequestQueue()
+
+        // Make url
+        assert(inciName != null)
+        val url = buildWikiUrlRequest(inciName!!)
+
+        // Make request to wikipedia, searching for ingredient informations.
+        val jsonObjectRequest =
+            JsonObjectRequest(Request.Method.GET, url, null, object : Response.Listener<JSONObject>() {
+
+                fun onResponse(response: JSONObject) {
+
+                    try {
+                        val query = response.get("query") as JSONObject
+                        val pages = query.get("pages") as JSONObject
+                        val keys = pages.keys()
+
+                        while (keys.hasNext()) {
+                            val key = keys.next()
+                            if (key != "-1" && pages.get(key) is JSONObject) {
+                                //show wikipedia extract
+                                val page = pages.get(key) as JSONObject
+                                val wikipediaExtract = page.get("extract") as String
+                                wikipediaView.setText(wikipediaExtract)
+                            } else {
+                                //wikipedia page not found
+                                wikipediaView.setText(R.string.wikipedia_not_found)
+                            }
+                        }
+
+                    } catch (e: JSONException) {
+                        Log.e(TAG, "invalid response from wikipedia")
+                        wikipediaView.setText(R.string.wikipedia_failed_request)
+                    }
+
+                }
+            }, object : Response.ErrorListener() {
+
+                fun onErrorResponse(error: VolleyError) {
+                    Log.e(TAG, "Could not send request to wikipedia")
+                    wikipediaView.setText(R.string.wikipedia_failed_request)
+                }
+            })
+
+        // Add the request to the RequestQueue.
+        RequestQueueSingleton.getInstance(getActivity()).addToRequestQueue(jsonObjectRequest)
+    }
+
+    /**
+     * Build URL for the request to wikipedia of an extract of the given ingredient in json format
+     * @param inciName Inci name of the ingredient to search on wikipedia
+     * @return Url string for the request
+     */
+    private fun buildWikiUrlRequest(inciName: String): String {
+        return "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&" +
+                "exintro&explaintext&redirects=1&titles=" + inciName.toLowerCase()
+    }
+
+    fun onResume() {
+        super.onResume()
+
+        //set dialog fragment size (width and height values in fragment_ingredients_details.xml do not work)
+        val window = getDialog().getWindow()
+        if (window != null) {
+            window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        }
+    }
+
+    companion object {
+
+        /**
+         * Method for the fragment creation
+         * @param inciName Title to show in the fragment
+         * @param description Description of the ingredient
+         * @param function Functions of the ingredient
+         * @return Returns the fragment created
+         */
+        fun newInstance(inciName: String, description: String, function: String): IngredientDetailsFragment {
+            val fragment = IngredientDetailsFragment()
+            val args = Bundle()
+            args.putString(ARG_NAME, inciName)
+            args.putString(ARG_DESCRIPTION, description)
+            args.putString(ARG_FUNCTION, function)
+            fragment.setArguments(args)
+            return fragment
+        }
+    }
+
+
+}
